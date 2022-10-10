b99a99392ed13aaa28d4be5077f4afb5724d39ce
Alison Sassi <alisonsassi@outlook.com>
2022-10-10 20:55:44 -0300

    heroku input

--- patient/importCSV.py
+++ patient/importCSV.py
@@ -4,14 +4,24 @@ conn = psycopg2.connect(database="dadijunvuu9gm3",
                         user='doxtibbjpjckmm', password='41ec522c36b1e43c28b1e830f4c82ceb4ff124707fae212f63b07654a85631d0', 
                         host='ec2-44-207-133-100.compute-1.amazonaws.com', port='5432'
 )
+
+
 cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM patient')
+all = cursor.fetchall()
+print("---all->")
+print(all)
 with open('NameFileExport.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader) # Skip the header row.
     for row in reader:
-        print("Passou aqui 2.")
+        print("list->")
+        print(list(row))
+        res = str(row)[1:-1]
+        print("res->")
+        print(res)
         insert_query = ("INSERT INTO patient (name_patient,neurological,MeaningNeurological,cardiovascular,MeaningCardiovascular,respiratory,MeaningRespiratory,coagulation,MeaningCoagulation,hepatic,MeaningHepatic,renal,MeaningRenal,spict,MeaningSpict,ecog,MeaningEcog,scoreSOFA,scoreAmib,group_patient,classification,active,exported,validatedDoctor,justification) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)", row)
-        print(list(insert_query))
         print("------------")
         print(insert_query)
         cursor.execute(list(insert_query))
