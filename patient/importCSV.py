fad88ed8d5f4525cb0175ca0af0d4296ae8b3be3
Alison Sassi <alisonsassi@outlook.com>
2022-10-10 20:28:39 -0300

    heroku input

--- patient/importCSV.py
+++ patient/importCSV.py
@@ -1,10 +1,24 @@
+import csv
 import psycopg2
-  
 conn = psycopg2.connect(database="dadijunvuu9gm3",
                         user='doxtibbjpjckmm', password='41ec522c36b1e43c28b1e830f4c82ceb4ff124707fae212f63b07654a85631d0', 
                         host='ec2-44-207-133-100.compute-1.amazonaws.com', port='5432'
 )
-  
+cur = conn.cursor()
+with open('NameFileExport.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row.
+    for row in reader:
+        cur.execute(
+        "INSERT INTO patient (name_patient,neurological,MeaningNeurological,cardiovascular,MeaningCardiovascular,respiratory,MeaningRespiratory,coagulation,MeaningCoagulation,hepatic,MeaningHepatic,renal,MeaningRenal,spict,MeaningSpict,ecog,MeaningEcog,scoreSOFA,scoreAmib,group_patient,classification,active,exported,validatedDoctor,justification) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)",
+        row
+    )
+conn.commit()
+
+
+
+
+
 conn.autocommit = True
 cursor = conn.cursor()
   
@@ -14,6 +28,7 @@ sql = '''SELECT * FROM patient;'''
 cursor.execute(sql) 
 
 for i in cursor.fetchall():
+    print("entrou!")
     print(i)
 
   
