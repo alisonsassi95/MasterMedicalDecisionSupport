2267cf7be0021c92ae303d3af3bf881c2360e334
Alison Sassi <alisonsassi@outlook.com>
2022-10-10 20:58:14 -0300

    heroku input

--- patient/importCSV.py
+++ patient/importCSV.py
@@ -16,16 +16,14 @@ with open('NameFileExport.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader) # Skip the header row.
     for row in reader:
-        print("list->")
-        print(list(row))
-        res = str(row)[1:-1]
-        print("res->")
-        print(res)
-        insert_query = ("INSERT INTO patient (name_patient,neurological,MeaningNeurological,cardiovascular,MeaningCardiovascular,respiratory,MeaningRespiratory,coagulation,MeaningCoagulation,hepatic,MeaningHepatic,renal,MeaningRenal,spict,MeaningSpict,ecog,MeaningEcog,scoreSOFA,scoreAmib,group_patient,classification,active,exported,validatedDoctor,justification) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)", row)
+        tran = str(row)[1:-1]
+        print("tran->")
+        print(tran)
+        insert_query = ("INSERT INTO patient (name_patient,neurological,MeaningNeurological,cardiovascular,MeaningCardiovascular,respiratory,MeaningRespiratory,coagulation,MeaningCoagulation,hepatic,MeaningHepatic,renal,MeaningRenal,spict,MeaningSpict,ecog,MeaningEcog,scoreSOFA,scoreAmib,group_patient,classification,active,exported,validatedDoctor,justification) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)", tran)
         print("------------")
         print(insert_query)
         cursor.execute(list(insert_query))
-        print("Passou aqui 3.")
+        print("Uhuulll")
 conn.commit()
 cursor.close()
 conn.close()
